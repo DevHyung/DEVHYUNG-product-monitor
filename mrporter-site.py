a3748511f674644efe11a1e1c49e3dd164f72f63
DevHyung <khuphj@gmail.com>
2018-08-25 04:45:59 +0900

    끝

--- mrporter-site.py
+++ mrporter-site.py
@@ -1,6 +1,6 @@
 #-*-encoding:utf8:-*-
 ''' --------------------------------- INPUT YOUR CONFIG --------------------------------- '''
-MONITOR_DELAY = 10 # second, if your input 10, monitor interval 10 second
+MONITOR_DELAY = 5 # second, if your input 10, monitor interval 10 second
 discord_webhook = 'https://discordapp.com/api/webhooks/481411222118465550/8TanFM9unt2Ztf_ySUGlus9MNw9DVDaTKNXAQZpMYvtnoucHevzCYn0gjwV_ZpQmKsTQ'
 
 ''' ------------------------------------------------------------------------------------- '''
@@ -23,24 +23,26 @@ def send_embed(alert_type,product):
     embed = Webhook(url, color=123123)
 
     # Set author info
-    embed.set_author(name='NERYS', icon='https://static.zerochan.net/Daenerys.Targaryen.full.2190849.jpg')
+    embed.set_author(name='Mrporter', icon='https://previews.123rf.com/images/martialred/martialred1604/martialred160400080/55731598-%EB%A9%94%EC%8B%9C%EC%A7%80-%EC%95%B1%EA%B3%BC-%EC%9B%B9-%EC%82%AC%EC%9D%B4%ED%8A%B8%EC%97%90-%EB%8C%80%ED%95%9C-chatbot-%EC%B1%84%ED%8C%85-%EB%B4%87-%EB%98%90%EB%8A%94-%EC%B1%84%ED%84%B0-%EB%B4%87-%EB%9D%BC%EC%9D%B8-%EC%95%84%ED%8A%B8-%EC%95%84%EC%9D%B4%EC%BD%98.jpg')
 
     # Set product details
     if(alert_type == "RESTOCK"):
         embed.set_desc("RESTOCK: " + "title")
     elif(alert_type == "NEW"):
-        embed.set_desc("NEW: " + "title")
+        embed.set_desc("NEW: " + product.name)
 
-    embed.add_field(name="Product", value="유현딱지사랑해")
-    embed.add_field(name="Link", value="link")
-    embed.add_field(name="Stock", value="stock")
+    embed.add_field(name="Product", value=product.name)
+    embed.add_field(name="Brand", value=product.brand)
+    embed.add_field(name="Price", value=product.price)
+    embed.add_field(name="Link", value=product.siteLik)
+    embed.add_field(name="ImgLink", value=product.link)
 
     # Set product image
-    embed.set_thumbnail('https://static.zerochan.net/Daenerys.Targaryen.full.2190849.jpg')
-    embed.set_image('https://static.zerochan.net/Daenerys.Targaryen.full.2190849.jpg')
+    #embed.set_thumbnail('https://cache.mrporter.com/images/products/1054353/1054353_mrp_fr_l.jpg')
+    #embed.set_image(product.link)
 
     # Set footer
-    embed.set_footer(text='NERYS by @snivynGOD', icon='https://static.zerochan.net/Daenerys.Targaryen.full.2190849.jpg', ts=True)
+    embed.set_footer(text='Mrporter by @DevHong', icon='https://static.zerochan.net/Daenerys.Targaryen.full.2190849.jpg', ts=True)
 
     # Send Discord alert
     embed.post()
@@ -51,6 +53,7 @@ class Product:
         self.name = name
         self.price = price
         self.siteLik = siteLik
+
 def read_from_txt(path):
     '''
     (None) -> list of str
@@ -177,7 +180,7 @@ def build_db():
             imgLink = li.find('div', class_=IMG_CLASS_NAME).img['src']
             siteLink = li.a['href']
             #print(DEBUG_PRINT.format(brand, name, price, imgLink, SITE + siteLink))
-            products_list[siteLink] = Product(brand,name,price,imgLink,SITE + siteLink)
+            products_list[siteLink] = Product(brand, name, price, 'https://' + imgLink[2:], SITE + siteLink)
     #
 
 def monitor():
@@ -227,7 +230,7 @@ def monitor():
             products_list[siteLink]
         except:
             log('s', "Added " + name + " to the database.")
-            products_list[siteLink] = Product(brand, name, price, imgLink, SITE + siteLink)
+            products_list[siteLink] = Product(brand, name, price, 'https://' + imgLink[2:], SITE + siteLink)
             send_embed('NEW',products_list[siteLink])
 
 
@@ -258,8 +261,5 @@ if __name__ == "__main__":
     while (True):
         monitor()
         time.sleep(MONITOR_DELAY)
-        idx +=1
-        if idx == 3:
-            products_list.pop('/en-jp/mens/gucci/striped-rubber-slides/1054353')
 
 
